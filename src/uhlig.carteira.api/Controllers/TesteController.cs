57a66e361e1386561483efaf0a7d03bece92659f
Mauricio Uhlig <mauriciojastrowuhlig@gmail.com>
2022-05-13 01:03:10 -0300

    Migração de .NET Core 3.1 para .NET 6

---
+++ src/uhlig.carteira.api/Controllers/TesteController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TesteController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<object> _logger;
+
+    public TesteController(ILogger<object> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<object> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
